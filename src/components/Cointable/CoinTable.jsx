d935cbf63bda41a63cf7f5c1eee567ed32b616ea
Akashkr28 <email.akash2010@gmail.com>
2024-08-12 22:32:33 +0530

    fetch coin added

--- src/components/Cointable/CoinTable.jsx
+++ src/components/Cointable/CoinTable.jsx
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react";
+import { fetchCoinData } from "../../services/fetchCoinData";
 
 function CoinTable() {
+    //useEffect function is called up to fetch "fetchCoinData.js" to perform its respective function
+    useEffect(() =>{
+        fetchCoinData(1, 'usd')
+    },[]);
 
     return (
+          
         <>Coin Table</>
     )
 
--- src/services/fetchCoinData.js
+++ src/services/fetchCoinData.js
@@ -1,9 +1,10 @@
 import axiosinstance from "../helpers/axiosinstance";
 
 
-export async function fetchCoinData() {
+export async function fetchCoinData(page = 1, currency = 'usd') {
+    const perPage = 10;
     try{
-        const response = await axiosinstance.get('/coins/markets?vs_currency=usd');
+        const response = await axiosinstance.get(`/coins/markets?vs_currency=${currency}&order=market_cap_dedc&per_page=${perPage}&page=${page}`);
         console.log(response); //to show the working
         return response;
     }catch(error){
