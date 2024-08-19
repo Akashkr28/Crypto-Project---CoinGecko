1267a4a81228ebc66a2928a5f61d7f7664e71cd3
Akashkr28 <email.akash2010@gmail.com>
2024-08-20 04:59:38 +0530

    added layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import './App.css'
 import Routing from './components/Routing/Routing'
-// import { useState } from 'react'
-import Home from './pages/Home'
-// import { CurrencyContext } from './context/CurrencyContext'
 
 function App() {
-
-  // const [currency, setCurrency] = useState('usd')
-  
-
   return (
-    <>
-      {/* <CurrencyContext.Provider value={ {currency, setCurrency} }> */}
+    <>  
       <Routing/>
-      {/* </CurrencyContext.Provider> */}
+      
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import Banner from "../components/Banner/Banner"
 import CoinTable from "../components/Cointable/CoinTable";
-import Navbar from "../components/Navbar/Navbar";
+
 
 
 function Home() {
     return(
-        <>
-        <Navbar/>
+      <>
+        
         <Banner/>
         <CoinTable/>
       </>
