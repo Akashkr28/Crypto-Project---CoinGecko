447cba3e873cc4d26203f4f0e2f47903900f1974
Akashkr28 <email.akash2010@gmail.com>
2024-08-20 07:20:31 +0530

    fixed syntaxtical error on Rounting

--- src/components/Routing/Routing.jsx
+++ src/components/Routing/Routing.jsx
@@ -6,12 +6,12 @@ import MainLayout from "../../pages/Layout";
 function Routing() {
 
     return (
-        <Routes>
-            <Route path="/" element={<MainLayout />}/>
-                <Route index element={<Home/>}/> 
-                <Route path="/details/:coinId" element={<CoinDetailsPage/>}/>
-            <Route/>            
-        </Routes>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={<Home/>}/> 
+                    <Route path="/details/:coinId" element={<CoinDetailsPage/>}/>
+                </Route>            
+            </Routes>
     );
 }
 
