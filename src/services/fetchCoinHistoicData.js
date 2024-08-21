26e69c33889ba36c9b6773e21d64b9fb8aa28bce
Akashkr28 <email.akash2010@gmail.com>
2024-08-22 05:07:43 +0530

    Added charts

---
+++ src/services/fetchCoinHistoicData.js
@@ -0,0 +1,11 @@
+import axiosinstance from "../helpers/axiosinstance";
+
+export async function fetchCoinHistoricData(id, interval, days = 7, currency = 'usd' ){
+    try{
+        const response = await axiosinstance.get(`/coins/${id}/market_chart?days=${days}&vs_currency=${currency}&interval=${interval}`);
+        return response.data;
+    }catch(error){
+        console.error(error);
+        return null;
+    }
+}
